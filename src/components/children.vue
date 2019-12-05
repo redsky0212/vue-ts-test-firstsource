02cf0c0f16a63ad73db90dcb2a64c0628c68999c
redsky0212 <hyun0238@gmail.com>
2019-12-05 16:03:10 +0900

    자식 component에서 props로 받기 테스트

---
+++ src/components/children.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{parentMessage}}  
+  </div>  
+</template>
+
+<script lang="ts">
+import {Component, Vue, Prop} from 'vue-property-decorator';
+
+@Component
+export default class Children extends Vue {
+  @Prop() parentMessage?: string;
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,23 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <message />
+    <children :parentMessage="message" />
   </div>
 </template>
 
 <script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
 // @ is an alias to /src
 import message from '@/components/message.vue';
+import children from '@/components/children.vue';
 
-export default {
- components: {
+@Component({
+  components: {
     message,
+    children,
   },
-};
+})
+export default class Home extends Vue {
+  message: string = 'fjdsalkfjdskal';
+}
 </script>
