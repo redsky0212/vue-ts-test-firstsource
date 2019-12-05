40ae31c86628546956510ffb3f454c4f32562681
redsky0212 <hyun0238@gmail.com>
2019-12-05 17:16:10 +0900

    추가

--- src/components/children.vue
+++ src/components/children.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
+    {{changeMessage}}<hr />
     {{parentMessage}}  
   </div>  
 </template>
 
 <script lang="ts">
-import {Component, Vue, Prop} from 'vue-property-decorator';
+import {Component, Vue, Prop, Watch} from 'vue-property-decorator';
 
 @Component
 export default class Children extends Vue {
   @Prop() parentMessage?: string;
+
+  changeMessage: string = '';
+
+  @Watch('parentMessage')
+  messageChange(newVal: string, oldVal: string){
+    this.changeMessage = `${newVal} : ${oldVal}`;
+  }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <message />
     <children :parentMessage="message" />
+    <button @click="onChange">change</button>
   </div>
 </template>
 
@@ -20,5 +21,9 @@ import children from '@/components/children.vue';
 })
 export default class Home extends Vue {
   message: string = 'fjdsalkfjdskal';
+
+  onChange() {
+    this.message = '내용 바꾸기!!';
+  }
 }
 </script>
